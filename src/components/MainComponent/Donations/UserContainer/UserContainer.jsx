7814b884e512b6e73e924102f22b25ddb04bdfee
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-04-30 10:18:23 +0200

    add/demo_redux_thunk

--- src/components/MainComponent/Donations/UserContainer/UserContainer.jsx
+++ src/components/MainComponent/Donations/UserContainer/UserContainer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
+import {useSelector,useDispatch} from 'react-redux';
+import {fetchUsers} from '../../../../redux'
+
 
 const UserContainer = () => {
-  return <div>UserContainer</div>;
+  const userData = useSelector(state => state.userR);
+  const dispatch = useDispatch();
+  const [load, setLoad] = useState(false)
+
+  useEffect(() => {
+    
+    if(load) {
+      dispatch(fetchUsers())}
+  }, [load,dispatch])
+
+  return <div>
+    <h2>Lista de usuarios</h2>
+    {load===false?<button onClick={()=>setLoad(true)}>Descargar</button>:""}
+    {userData.loading?<p>Cargando....</p>
+      :userData.error?(<h2>{userData.error}</h2>)
+      :userData.users.map((user,i)=><p key={i}>{user.name}</p>)
+  }
+  </div>;
 };
 
 export default UserContainer;
